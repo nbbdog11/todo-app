cadf5abef603cea57a52fced75792a1ef3afd6c9
Scott Wagner <scottywags16@gmail.com>
2017-09-29 22:35:03 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+      },
+    }],
+  },
+  devServer: {
+    publicPath: '/dist/',
+  },
+};
