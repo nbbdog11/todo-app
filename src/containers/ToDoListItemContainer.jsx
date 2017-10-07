d3de42504fb1ade77aa10a91594e3f08e097c8eb
Scott Wagner <scottywags16@gmail.com>
2017-10-06 20:35:57 -0400

    remove unused import

--- src/containers/ToDoListItemContainer.jsx
+++ src/containers/ToDoListItemContainer.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
   deleteTodo,
-  editTodo,
   saveEdit,
 } from '../state/actions/actionCreators';
 import ToDoListItem from '../components/ToDoListItem';
