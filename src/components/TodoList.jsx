a7e1a6f8102432198598afc660cba5d37624253f
Scott Wagner <scottywags16@gmail.com>
2017-10-16 22:45:24 -0400

    show count and percentage of completed todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,13 +14,34 @@ const buildTodoListItem = item => (
 const buildTodoListItems = items =>
   items.map(item => buildTodoListItem(item));
 
-const TodoList = props => (
-  <div>
-    <p>{`List size: ${props.todos.length}`}</p>
-    <AddTodoFormContainer />
-    {buildTodoListItems(props.todos)}
-  </div>
-);
+const getCompletionStats = (todos) => {
+  const completedCount = todos.filter(todo => todo.completed).length;
+  const totalCount = todos.length;
+  const completedPercentage = totalCount === 0 ? 0 : (completedCount / totalCount) * 100;
+  return {
+    completedCount,
+    completedPercentage,
+    totalCount,
+  };
+};
+
+const TodoList = (props) => {
+  const {
+    completedCount,
+    completedPercentage,
+    totalCount,
+  } = getCompletionStats(props.todos);
+
+  const completionStatsString = `Completed ${completedPercentage.toFixed(2)}%: ${completedCount}/${totalCount}`;
+
+  return (
+    <div>
+      <p>{completionStatsString}</p>
+      <AddTodoFormContainer />
+      {buildTodoListItems(props.todos)}
+    </div>
+  );
+};
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object),
