0bb1b32893305d05f6b6c1ab90aed8bdee9c423d
Scott Wagner <scottywags16@gmail.com>
2017-11-16 21:38:39 -0500

    add tests for api module

--- __test__/helpers/api.test.js
+++ __test__/helpers/api.test.js
@@ -1,13 +1,46 @@
 import {
+  deleteStore,
+  getStore,
+} from '../../src/state/store/store';
+import {
   getNameForList,
 } from '../../src/helpers/api';
 
-describe('api', () => {
-  describe('getNameForList', () => {
-    it('returns "List" when id does not exist in state', () => {
-      const result = getNameForList();
+const preSetStateInStore = state => getStore(state);
 
-      expect(result).toBe('List');
+describe('api', () => {
+  afterEach(deleteStore);
+  describe('getNameForList', () => {
+    describe('returns "List"', () => {
+      it('when id is not passed in', () => {
+        const result = getNameForList();
+
+        expect(result).toBe('List');
+      });
+
+      it('when id does not exist in state', () => {
+        const nonExistentId = 'some-id';
+
+        const result = getNameForList(nonExistentId);
+
+        expect(result).toBe('List');
+      });
+    });
+
+    it('returns name of list when id is in state', () => {
+      const id = 'id';
+      const list = {
+        name: 'name',
+      };
+      const lists = new Map([[id, list]]);
+      const state = {
+        lists,
+      };
+      preSetStateInStore(state);
+
+      const result = getNameForList(id);
+
+      expect(result).toBe(list.name);
     });
   });
 });
