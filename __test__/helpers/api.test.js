620a6f994bd85f99b0de100a1ae7295fcbeb624c
Scott Wagner <scottywags16@gmail.com>
2017-11-18 10:53:16 -0500

    stop showing decimal points for integer copmleted percentage values

--- __test__/helpers/api.test.js
+++ __test__/helpers/api.test.js
@@ -3,6 +3,7 @@ import {
   getStore,
 } from '../../src/state/store/store';
 import {
+  getCompletionStatsForList,
   getNameForList,
 } from '../../src/helpers/api';
 
@@ -41,6 +42,125 @@ describe('api', () => {
       const result = getNameForList(id);
 
       expect(result).toBe(list.name);
+    });
+  });
+
+  describe('getCompletionStatsForList', () => {
+    describe('completedCount', () => {
+      it('is 0 when there are no todos', () => {
+        const result = getCompletionStatsForList('some-list');
+
+        expect(result.completedCount).toBe(0);
+      });
+
+      it('is 0 when no todos in list are completed', () => {
+        const listId = 'id';
+        const incompleteTodo = {
+          listId,
+          completed: false,
+        };
+        const todos = [incompleteTodo];
+        const state = {
+          todos,
+        };
+        preSetStateInStore(state);
+
+        const result = getCompletionStatsForList(listId);
+
+        expect(result.completedCount).toBe(0);
+      });
+
+      it('equals number of completed todos in list when any are completed', () => {
+        const listId = 'id';
+        const incompleteTodo = {
+          listId,
+          completed: false,
+        };
+        const completeTodo = {
+          listId,
+          completed: true,
+        };
+        const completeTodoForOtherList = {
+          listId: 'some-other-list',
+          completed: true,
+        };
+        const todos = [incompleteTodo, completeTodo, completeTodoForOtherList];
+        const state = {
+          todos,
+        };
+        preSetStateInStore(state);
+
+        const result = getCompletionStatsForList(listId);
+
+        expect(result.completedCount).toBe(1);
+      });
+    });
+
+    describe('completedPercentage', () => {
+      it('is 0 when there are no todos in list', () => {
+        const result = getCompletionStatsForList('some-list');
+
+        expect(result.completedPercentage).toBe('0');
+      });
+
+      it('is 0 when there are no completed todos in list', () => {
+        const listId = 'list-id';
+        const incompleteTodo = {
+          listId,
+          completed: false,
+        };
+        const completeTodoForOtherList = {
+          listId: 'some-other-list-id',
+          completed: true,
+        };
+        const todos = [incompleteTodo, completeTodoForOtherList];
+        const state = {
+          todos,
+        };
+        preSetStateInStore(state);
+
+        const result = getCompletionStatsForList(listId);
+
+        expect(result.completedPercentage).toBe('0');
+      });
+
+      it('is 100 when there are only completed todos in list', () => {
+        const listId = 'list-id';
+        const completeTodo = {
+          listId,
+          completed: true,
+        };
+        const todos = [completeTodo];
+        const state = {
+          todos,
+        };
+        preSetStateInStore(state);
+
+        const result = getCompletionStatsForList(listId);
+
+        expect(result.completedPercentage).toBe('100');
+      });
+
+      it('uses 2 decimal places when the percentage is not a whole number', () => {
+        const listId = 'list-id';
+        const completeTodo = {
+          listId,
+          completed: true,
+        };
+        const incompleteTodo = {
+          listId,
+          completed: false,
+        };
+        const todos = [completeTodo, incompleteTodo, incompleteTodo];
+        const state = {
+          todos,
+        };
+        preSetStateInStore(state);
+
+        const result = getCompletionStatsForList(listId);
+
+        expect(result.completedPercentage).toBe('33.33');
+      });
     });
   });
 });
